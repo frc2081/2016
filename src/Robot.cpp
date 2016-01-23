e7124d17a0b078b05d7ca282e9c72e6a408c35a0
Matthew <Matthew@192.168.2.191>
2016-01-23 12:35:25 -0600

    Completed button map

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,7 +1,7 @@
 #include "WPILib.h"
+#include <iostream>
 
-class Robot: public IterativeRobot
-{
+class Robot: public IterativeRobot {
 private:
 	LiveWindow *lw = LiveWindow::GetInstance();
 	SendableChooser *chooser;
@@ -9,8 +9,36 @@ private:
 	const std::string autoNameCustom = "My Auto";
 	std::string autoSelected;
 
+	Joystick *stick;
+	RobotDrive *drive;
+	JoystickButton *button1; //A
+	JoystickButton *button2; //B
+	JoystickButton *button3; //X
+	JoystickButton *button4; //Y
+	JoystickButton *button5; //Left Bumper
+	JoystickButton *button6; //Right Bumper
+	JoystickButton *button7; //Back
+	JoystickButton *button8; //Start
+	JoystickButton *button9; //Left Stick Depress
+	JoystickButton *button10; //Right Stick Depress
+
 	void RobotInit()
 	{
+
+		stick = new Joystick(0);
+		drive = new RobotDrive(0, 1);
+
+		button1 = new JoystickButton(stick, 1),
+		button2 = new JoystickButton(stick, 2),
+		button3 = new JoystickButton(stick, 3),
+		button4 = new JoystickButton(stick, 4),
+		button5 = new JoystickButton(stick, 5),
+		button6 = new JoystickButton(stick, 6),
+		button7 = new JoystickButton(stick, 7),
+		button8 = new JoystickButton(stick, 8),
+		button9 = new JoystickButton(stick, 9),
+		button10 = new JoystickButton(stick, 10);
+
 		chooser = new SendableChooser();
 		chooser->AddDefault(autoNameDefault, (void*)&autoNameDefault);
 		chooser->AddObject(autoNameCustom, (void*)&autoNameCustom);
@@ -56,7 +84,20 @@ private:
 
 	void TeleopPeriodic()
 	{
+		double LaxisX;
+		double LaxisY;
 
+		double RaxisX;
+		double RaxisY;
+
+		LaxisX = stick->GetX();
+		LaxisY = stick->GetY();
+
+		RaxisX = stick->GetTwist();
+		RaxisY = stick->GetRawAxis(5);
+
+		drive->ArcadeDrive(LaxisY, RaxisX);
+		//printf("drive %.3f", LaxisY);
 	}
 
 	void TestPeriodic()
