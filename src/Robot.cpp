8d1e7e3a83bfb6aa254e0fc05a56b20e507ab120
Matthew <Matthew@192.168.2.191>
2016-01-23 14:04:06 -0600

    Working buttons w/ output to log :)

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -12,16 +12,18 @@ private:
 
 	Joystick *stick;
 	RobotDrive *drive;
-	JoystickButton *button1; //A
-	JoystickButton *button2; //B
-	JoystickButton *button3; //X
-	JoystickButton *button4; //Y
-	JoystickButton *button5; //Left Bumper
-	JoystickButton *button6; //Right Bumper
-	JoystickButton *button7; //Back
-	JoystickButton *button8; //Start
-	JoystickButton *button9; //Left Stick Depress
-	JoystickButton *button10; //Right Stick Depress
+	JoystickButton *buttonA;
+	JoystickButton *buttonB;
+	JoystickButton *buttonX;
+	JoystickButton *buttonY;
+	JoystickButton *buttonLB;
+	JoystickButton *buttonRB;
+	JoystickButton *buttonBack;
+	JoystickButton *buttonStart;
+	JoystickButton *buttonLS;
+	JoystickButton *buttonRS;
+
+	bool bA, bB, bX, bY, bLB, bRB, bBack, bStart, bLS, bRS;
 
 	void RobotInit()
 	{
@@ -29,16 +31,16 @@ private:
 		stick = new Joystick(0);
 		drive = new RobotDrive(0, 1);
 
-		button1 = new JoystickButton(stick, 1),
-		button2 = new JoystickButton(stick, 2),
-		button3 = new JoystickButton(stick, 3),
-		button4 = new JoystickButton(stick, 4),
-		button5 = new JoystickButton(stick, 5),
-		button6 = new JoystickButton(stick, 6),
-		button7 = new JoystickButton(stick, 7),
-		button8 = new JoystickButton(stick, 8),
-		button9 = new JoystickButton(stick, 9),
-		button10 = new JoystickButton(stick, 10);
+		buttonA = new JoystickButton(stick, 1),
+		buttonB = new JoystickButton(stick, 2),
+		buttonX = new JoystickButton(stick, 3),
+		buttonY = new JoystickButton(stick, 4),
+		buttonLB = new JoystickButton(stick, 5),
+		buttonRB = new JoystickButton(stick, 6),
+		buttonBack = new JoystickButton(stick, 7),
+		buttonStart = new JoystickButton(stick, 8),
+		buttonLS = new JoystickButton(stick, 9),
+		buttonRS = new JoystickButton(stick, 10);
 
 		chooser = new SendableChooser();
 		chooser->AddDefault(autoNameDefault, (void*)&autoNameDefault);
@@ -83,12 +85,23 @@ private:
 
 	}
 
+	void checkbuttons() {
+		bA = stick->GetRawButton(1);
+		bB = stick->GetRawButton(2);
+		bX = stick->GetRawButton(3);
+		bY = stick->GetRawButton(4);
+		bLB = stick->GetRawButton(5);
+		bRB = stick->GetRawButton(6);
+		bBack = stick->GetRawButton(7);
+		bStart = stick->GetRawButton(8);
+		bLS = stick->GetRawButton(9);
+		bRS = stick->GetRawButton(10);
+	}
+
 	void TeleopPeriodic()
 	{
-		bool button1, button2, button3, button4, button5, button6, button7, button8, button9, button10;
-
+		checkbuttons();
 		double LaxisX, LaxisY;
-
 		double RaxisX, RaxisY;
 
 		LaxisX = stick->GetX();
@@ -98,34 +111,35 @@ private:
 		RaxisY = stick->GetRawAxis(5);
 
 		drive->ArcadeDrive(LaxisY, RaxisX);
-		if(button1 == 1) {
+
+		if(bA == TRUE) {
 			printf("A\n");
 		}
-		if(button2 == TRUE) {
+		if(bB == TRUE) {
 			printf("B\n");
 		}
-		if(button3 == TRUE) {
+		if(bX == TRUE) {
 			printf("X\n");
 		}
-		if(button4 == TRUE) {
+		if(bY == TRUE) {
 			printf("Y\n");
 		}
-		if(button5 == TRUE) {
+		if(bLB == TRUE) {
 			printf("LBumper\n");
 		}
-		if(button6 == TRUE) {
+		if(bRB == TRUE) {
 			printf("RBumper\n");
 		}
-		if(button7 == TRUE) {
+		if(bBack == TRUE) {
 			printf("Back\n");
 		}
-		if(button8 == TRUE) {
+		if(bStart == TRUE) {
 			printf("Start\n");
 		}
-		if(button9 == TRUE) {
+		if(bLS == TRUE) {
 			printf("LStick\n");
 		}
-		if(button10 == TRUE) {
+		if(bRS == TRUE) {
 			printf("RStick\n");
 		}
 
