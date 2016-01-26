c06dafb614c843cd9bbbc97216208871c1a2bfa8
Matthew <Matthew@192.168.2.56>
2016-01-25 18:51:04 -0600

    Update Commit

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,7 +1,8 @@
 #include "WPILib.h"
+#include <iostream>
+#include <string>
 
-class Robot: public IterativeRobot
-{
+class Robot: public IterativeRobot {
 private:
 	LiveWindow *lw = LiveWindow::GetInstance();
 	SendableChooser *chooser;
@@ -9,10 +10,24 @@ private:
 	const std::string autoNameCustom = "My Auto";
 	std::string autoSelected;
 
+	Joystick *stick;
+	RobotDrive *drive;
+	JoystickButton *buttonA;
+	JoystickButton *buttonB;
+	JoystickButton *buttonX;
+	JoystickButton *buttonY;
+	JoystickButton *buttonLB;
+	JoystickButton *buttonRB;
+	JoystickButton *buttonBack;
+	JoystickButton *buttonStart;
+	JoystickButton *buttonLS;
+	JoystickButton *buttonRS;
+
+	bool bA, bB, bX, bY, bLB, bRB, bBack, bStart, bLS, bRS;
+	
 	Encoder*LEnc;
 	Encoder*REnc;
 	Encoder*ArmEnc;
-	Joystick *stick;
 	float count;
 
 	VictorSP *mot;
@@ -38,6 +53,16 @@ private:
 
 	void RobotInit()
 	{
+		buttonA = new JoystickButton(stick, 1),
+		buttonB = new JoystickButton(stick, 2),
+		buttonX = new JoystickButton(stick, 3),
+		buttonY = new JoystickButton(stick, 4),
+		buttonLB = new JoystickButton(stick, 5),
+		buttonRB = new JoystickButton(stick, 6),
+		buttonBack = new JoystickButton(stick, 7),
+		buttonStart = new JoystickButton(stick, 8),
+		buttonLS = new JoystickButton(stick, 9),
+		buttonRS = new JoystickButton(stick, 10);
 
 		chooser = new SendableChooser();
 		chooser->AddDefault(autoNameDefault, (void*)&autoNameDefault);
@@ -96,12 +121,68 @@ private:
 	{
 
 	}
+	
+	void checkbuttons() {
+		bA = stick->GetRawButton(1);
+		bB = stick->GetRawButton(2);
+		bX = stick->GetRawButton(3);
+		bY = stick->GetRawButton(4);
+		bLB = stick->GetRawButton(5);
+		bRB = stick->GetRawButton(6);
+		bBack = stick->GetRawButton(7);
+		bStart = stick->GetRawButton(8);
+		bLS = stick->GetRawButton(9);
+		bRS = stick->GetRawButton(10);
+	}
 
 	void TeleopPeriodic()
 	{
 		float RTrig;
 		float LTrig;
 		float Trig;
+		
+		checkbuttons();
+		double LaxisX, LaxisY;
+		double RaxisX, RaxisY;
+
+		LaxisX = stick->GetX();
+		LaxisY = stick->GetY();
+
+		RaxisX = stick->GetTwist();
+		RaxisY = stick->GetRawAxis(5);
+
+		drive->ArcadeDrive(LaxisY, RaxisX);
+		//Prints button inputs to logs
+		/*if(bA == TRUE) {
+			printf("A\n");
+		}
+		if(bB == TRUE) {
+			printf("B\n");
+		}
+		if(bX == TRUE) {
+			printf("X\n");
+		}
+		if(bY == TRUE) {
+			printf("Y\n");
+		}
+		if(bLB == TRUE) {
+			printf("LBumper\n");
+		}
+		if(bRB == TRUE) {
+			printf("RBumper\n");
+		}
+		if(bBack == TRUE) {
+			printf("Back\n");
+		}
+		if(bStart == TRUE) {
+			printf("Start\n");
+		}
+		if(bLS == TRUE) {
+			printf("LStick\n");
+		}
+		if(bRS == TRUE) {
+			printf("RStick\n");
+		}*/
 
 		RTrig = stick->GetRawAxis(3);
 		LTrig = stick->GetRawAxis(2);
